20a1082cf42f77000bacd7babb5439c5732f5ea9
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-10-21 11:01:59 +0300

    Updated

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -149,6 +149,63 @@ const getPost = async (req, res, next) => {
   }
 };
 
+// const getAllPosts = async (req, res, next) => {
+//   try {
+//     const filter = req.query.searchKeyword;
+//     const categories = req.query.categories
+//       ? req.query.categories.split(",")
+//       : []; // Expecting categories to be comma-separated
+
+//     let where = {};
+
+//     if (filter) {
+//       where.title = { $regex: filter, $options: "i" };
+//     }
+
+//     if (categories.length > 0) {
+//       where.categories = { $in: categories };
+//     }
+
+//     let query = Post.find(where);
+//     const page = parseInt(req.query.page) || 1;
+//     const pageSize = parseInt(req.query.limit) || 10;
+//     const skip = (page - 1) * pageSize;
+//     const total = await Post.find(where).countDocuments();
+//     const pages = Math.ceil(total / pageSize);
+
+//     res.header({
+//       "x-filter": filter,
+//       "x-totalcount": JSON.stringify(total),
+//       "x-currentpage": JSON.stringify(page),
+//       "x-pagesize": JSON.stringify(pageSize),
+//       "x-totalpagecount": JSON.stringify(pages),
+//     });
+
+//     if (page > pages) {
+//       return res.json([]);
+//     }
+
+//     const result = await query
+//       .skip(skip)
+//       .limit(pageSize)
+//       .populate([
+//         {
+//           path: "user",
+//           select: ["avatar", "name", "verified"],
+//         },
+//         {
+//           path: "categories",
+//           select: ["title"],
+//         },
+//       ])
+//       .sort({ updatedAt: "desc" });
+
+//     return res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 const getAllPosts = async (req, res, next) => {
   try {
     const filter = req.query.searchKeyword;
@@ -158,8 +215,12 @@ const getAllPosts = async (req, res, next) => {
 
     let where = {};
 
+    // `filter` ile hem `title` hem de `caption` alanında arama yapılır
     if (filter) {
-      where.title = { $regex: filter, $options: "i" };
+      where.$or = [
+        { title: { $regex: filter, $options: "i" } },
+        { caption: { $regex: filter, $options: "i" } },
+      ];
     }
 
     if (categories.length > 0) {
@@ -182,7 +243,7 @@ const getAllPosts = async (req, res, next) => {
     });
 
     if (page > pages) {
-      return res.json([]);
+      return res.json([]); // Eğer sayfa mevcut değilse boş döndür
     }
 
     const result = await query
